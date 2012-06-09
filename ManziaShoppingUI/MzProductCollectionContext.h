46c888de65da404f1843c8e08f1316dc37e96b30
Roy Manzi <roy.manzi@manzia.com>
2012-06-08 17:29:40 -0700

    Added code for the MzProductCollectionContext class...still needs extensive testing

--- ManziaShoppingUI/MzProductCollectionContext.h
+++ ManziaShoppingUI/MzProductCollectionContext.h
@@ -2,34 +2,38 @@
 //  MzProductCollectionContext.h
 //  ManziaShoppingUI
 //
-//  Created by Macbook Pro on 5/30/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Created by Roy Manzi Tumubweinee on 5/30/12.
+//  Copyright (c) 2012 Manzia Corporation. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
 @interface MzProductCollectionContext : NSManagedObjectContext {
-    NSString *_collectionCachePath;  
+    NSString *collectionCachePath;
+    NSString *collectionURLString;
 }
 
 // Properties
 @property (nonatomic, copy, readonly) NSString *collectionCachePath;
+@property (nonatomic, copy, readonly) NSString *collectionURLString;
 
 // path to product Images directory within CollectionCachePath
 
 @property (nonatomic, copy, readonly ) NSString *productImagesDirectoryPath;    
 
 // Initializer
-- (id)initWithCollectionURLString:(NSString *)URLString cachePath:(NSString *)collectionCachePath;
+- (id)initWithCollectionURLString:(NSString *)URLString cachePath:(NSString *)cachePath;
 
 // Returns a mutable request that's configured to do an HTTP GET operation 
 // for a resources with the given path relative to the collectionURLString. 
-// If path is nil, returns a request for the galleryURLString resource 
+// If path is nil, returns a request for the collectionURLString resource 
 // itself.  This can return fail (and return nil) if path is not nil and 
 // yet not a valid URL path.
 
 - (NSMutableURLRequest *)requestToGetCollectionRelativeString:(NSString *)path;
 
+// Const String declared in MzProductCollection.m
+extern NSString * kProductImagesDirectoryName;
 
 @end
--- ManziaShoppingUI/MzProductCollectionContext.m
+++ ManziaShoppingUI/MzProductCollectionContext.m
@@ -2,21 +2,74 @@
 //  MzProductCollectionContext.m
 //  ManziaShoppingUI
 //
-//  Created by Macbook Pro on 5/30/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Created by Roy Manzi Tumubweinee on 5/30/12.
+//  Copyright (c) 2012 Manzia Corporation. All rights reserved.
 //
 
 #import "MzProductCollectionContext.h"
+#import "NetworkManager.h"
 
 @interface MzProductCollectionContext()
 
+// private, read/write properties
 @property(nonatomic, copy, readwrite) NSString *collectionCachePath;
+@property(nonatomic, copy, readwrite) NSString *collectionURLString;
 
 @end
 
 @implementation MzProductCollectionContext 
-    
-    @synthesize collectionCachePath = _collectionCachePath;
 
+// Getters and setters
+@synthesize collectionCachePath;
+@synthesize collectionURLString;
+
+// Initialize
+- (id)initWithCollectionURLString:(NSString *)URLString cachePath:(NSString *)cachePath
+{
+    assert(URLString != nil);
+    assert(cachePath != nil);
+    
+    self = [super init];
+    if (self != nil) {
+        self->collectionURLString = [URLString copy];
+        self->collectionCachePath = [cachePath copy];
+    }
+    return self;
+}
+
+// Returns path to the productImage Directory
+- (NSString *)productImagesDirectoryPath
+{
+    return [self.collectionCachePath stringByAppendingPathComponent:kProductImagesDirectoryName];
+}
+
+- (NSMutableURLRequest *)requestToGetCollectionRelativeString:(NSString *)path
+{
+    NSMutableURLRequest *urlRequest;
+    NSURL *url;
+    
+    assert([NSThread isMainThread]);
+    assert(self.collectionURLString != nil);
+    
+    urlRequest = nil;
+    
+    // Construct the URL.
+    
+    url = [NSURL URLWithString:self.collectionURLString];
+    assert(url != nil);
+    if (path != nil) {
+        url = [NSURL URLWithString:path relativeToURL:url];               
+    }
+    
+    // Call down to the network manager so that it can set up its stuff 
+    // (notably the user agent string).
+    
+    if (url != nil) {
+        urlRequest = [[NetworkManager sharedManager] requestToGetURL:url];
+        assert(urlRequest != nil);
+    }
+    
+    return urlRequest;
+}
 
 @end
