40d4e4d06c47ef933af9e9dc71a88d110e043cec
Roy Manzi <roy.manzi@manzia.com>
2012-06-18 11:09:00 -0700

    New Test case class for the MzProductCollection class

---
+++ ManziaShoppingUITest/MzProductCollectionTest.m
@@ -0,0 +1,27 @@
+//
+//  MzProductCollectionTest.m
+//  ManziaShoppingUI
+//
+//  Created by Macbook Pro on 6/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MzProductCollectionTest.h"
+#import "MzProductCollection.h"
+
+@interface MzProductCollectionTest ()
+
+@property (nonatomic, strong, readwrite) MzProductCollection *productCollection;
+
+@end
+
+@implementation MzProductCollectionTest
+
+// Setup method
+- (void) setUp {
+    NSLog(@"%@ setup", self.name);
+    
+    
+}
+
+@end
