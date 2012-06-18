2a9b4ed064abc392c2250188ba4568548ce13b2b
Roy Manzi <roy.manzi@manzia.com>
2012-06-18 11:08:29 -0700

    New test case class for the App

---
+++ ManziaShoppingUITest/ManziaShoppingUITest.h
@@ -0,0 +1,13 @@
+//
+//  ManziaShoppingUITest.h
+//  ManziaShoppingUITest
+//
+//  Created by Macbook Pro on 6/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ManziaShoppingUITest : SenTestCase
+
+@end
---
+++ ManziaShoppingUITest/ManziaShoppingUITest.m
@@ -0,0 +1,32 @@
+//
+//  ManziaShoppingUITest.m
+//  ManziaShoppingUITest
+//
+//  Created by Macbook Pro on 6/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ManziaShoppingUITest.h"
+
+@implementation ManziaShoppingUITest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ManziaShoppingUITest");
+}
+
+@end
