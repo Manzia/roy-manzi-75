a5f4ea98a4879a2e862516e7fdaf986a1f672a8f
Roy Manzi <roy.manzi@manzia.com>
2012-07-24 07:52:45 -0700

    Added the MzTaskCollection class and related support classes

---
+++ ManziaShoppingUI/MzTaskParserOperation.h
@@ -0,0 +1,13 @@
+//
+//  MzTaskParserOperation.h
+//  ManziaShoppingUI
+//
+//  Created by Macbook Pro on 7/24/12.
+//  Copyright (c) 2012 Manzia Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MzTaskParserOperation : NSOperation
+
+@end
---
+++ ManziaShoppingUI/MzTaskParserOperation.m
@@ -0,0 +1,13 @@
+//
+//  MzTaskParserOperation.m
+//  ManziaShoppingUI
+//
+//  Created by Macbook Pro on 7/24/12.
+//  Copyright (c) 2012 Manzia Corporation. All rights reserved.
+//
+
+#import "MzTaskParserOperation.h"
+
+@implementation MzTaskParserOperation
+
+@end
