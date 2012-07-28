5ee3ec6cdc4276c47495d4bfc449d86b2d868e27
Roy Manzi <roy.manzi@manzia.com>
2012-07-27 23:27:07 -0700

    Completed the MzTaskCollectionContext class...pending testing

--- ManziaShoppingUI/MzTaskCollectionContext.m
+++ ManziaShoppingUI/MzTaskCollectionContext.m
@@ -7,7 +7,64 @@
 //
 
 #import "MzTaskCollectionContext.h"
+#import "NetworkManager.h"
+
+@interface MzTaskCollectionContext()
+
+// private, read/write properties
+@property(nonatomic, copy, readwrite) NSString *tasksCachePath;
+@property(nonatomic, copy, readwrite) NSString *tasksURLString;
+
+@end
 
 @implementation MzTaskCollectionContext
+
+// Getters and setters
+@synthesize tasksCachePath;
+@synthesize tasksURLString;
+
+// Initialize
+- (id)initWithTasksURLString:(NSString *)URLString cachePath:(NSString *)cachePath
+{
+    assert(URLString != nil);
+    assert(cachePath != nil);
+    
+    self = [super init];
+    if (self != nil) {
+        self->tasksURLString = [URLString copy];
+        self->tasksCachePath = [cachePath copy];
+    }
+    return self;
+}
+
+- (NSMutableURLRequest *)requestToGetCollectionRelativeString:(NSString *)path
+{
+    NSMutableURLRequest *urlRequest;
+    NSURL *url;
+    
+    assert([NSThread isMainThread]);
+    assert(self.tasksURLString != nil);
+    
+    urlRequest = nil;
+    
+    // Construct the URL.
+    
+    url = [NSURL URLWithString:self.tasksURLString];
+    assert(url != nil);
+    if (path != nil) {
+        url = [NSURL URLWithString:path relativeToURL:url];               
+    }
+    
+    // Call down to the network manager so that it can set up its stuff 
+    // (notably the user agent string).
+    
+    if (url != nil) {
+        urlRequest = [[NetworkManager sharedManager] requestToGetURL:url];
+        assert(urlRequest != nil);
+    }
+    
+    return urlRequest;
+}
+
 
 @end
