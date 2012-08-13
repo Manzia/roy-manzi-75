aa2000fb4986261abbb26975cfdabdee59c123b7
Roy Manzi <roy.manzi@manzia.com>
2012-08-12 18:32:47 -0700

    MzTaskAttribute - minor modification

--- ManziaShoppingUI/MzTaskAttribute.m
+++ ManziaShoppingUI/MzTaskAttribute.m
@@ -25,5 +25,33 @@
     [[QLog log] logOption:kLogOptionSyncDetails withFormat:@"Will delete TaskAttribute with name: %@", self.taskAttributeName]; 
 }
 
+// For some strange reason, maybe an IOS bug!!!, you cannot use the dynamic accessors or
+// KVC to traverse a one-to-one relationship whose inverse is to-many relationship. When we
+// retrieve the MzTaskAttribute objects, we can access the attributeOptions relationship
+// which is to-many but its inverse taskType relationship which is one-to-one returns nil
+// even though it has a non-nil value in the persistent store/MOC and its not a fault.
+/*-(MzTaskType *)getTaskTypeForTaskAttribute:(MzTaskAttribute *)attribute inManagedObjectContext:(NSManagedObjectContext *)context
+{
+    assert(attribute != nil);
+    assert(context != nil);
+    NSArray *taskArray;
+    NSError *taskError = NULL;
+    
+    NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:@"MzTaskType"];
+    assert(request != nil);
+    [request setRelationshipKeyPathsForPrefetching:[NSArray arrayWithObject:@"taskAttributes"]];
+    
+    // Iterate through all the TaskTypes to see which one has the TaskAttribute
+    taskArray = [context executeFetchRequest:request error:&taskError];
+    
+    // Log error
+    if (taskError) {
+        [[QLog log] logOption:kLogOptionSyncDetails withFormat:
+         @"Error fetching from MzTaskType entity to get MzTaskAttribute with error: %@", taskError.localizedDescription];
+        return nil;        
+    }
+    
+}*/
+
 
 @end
