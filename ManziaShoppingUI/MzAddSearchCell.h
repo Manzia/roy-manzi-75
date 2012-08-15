3e515ede7e47f29708c91a08a83bcf289b541d6f
Roy Manzi <roy.manzi@manzia.com>
2012-08-14 22:24:18 -0700

    Added the MzAddSearchCell class

---
+++ ManziaShoppingUI/MzAddSearchCell.h
@@ -0,0 +1,18 @@
+//
+//  MzAddSearchCell.h
+//  ManziaShoppingUI
+//
+//  Created by Macbook Pro on 8/14/12.
+//  Copyright (c) 2012 Manzia Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MzAddSearchCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UIButton *leftOptionButton;
+@property (nonatomic, weak) IBOutlet UIButton *middleOptionButton;
+@property (nonatomic, weak) IBOutlet UIButton *rightOptionButton;
+        
+
+@end
---
+++ ManziaShoppingUI/MzAddSearchCell.m
@@ -0,0 +1,34 @@
+//
+//  MzAddSearchCell.m
+//  ManziaShoppingUI
+//
+//  Created by Macbook Pro on 8/14/12.
+//  Copyright (c) 2012 Manzia Corporation. All rights reserved.
+//
+
+#import "MzAddSearchCell.h"
+
+@implementation MzAddSearchCell
+
+@synthesize leftOptionButton;
+@synthesize middleOptionButton;
+@synthesize rightOptionButton;
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code - add the buttons as subviews
+             
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
